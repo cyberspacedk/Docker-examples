8aa4511bff091b6746ad63830dad3bc6f19d532e
cyberspacedk <frontend.dk@gmail.com>
2022-03-02 17:38:29 +0200

    add volumes

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const { port } = require('./config')
+const { connectDB } = require('./helpers/db')
+
+const postSchema = new mongoose.Schema({
+  title: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+const startServer = () => {
+  app.listen(port, () => {
+
+    console.log('Server started at 9000 port');
+
+    const createdPost = new Post({
+      title: 'Post title'
+    });
+
+    console.log('DB POST WITH VOLUME', createdPost)
+
+  });
+}
 
 app.get('/test', (req, res) => {
   res.send('Api work')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log('Server started at 9000 port')
-})
+connectDB()
+  .on('error', console.error)
+  .on('disconnect', connectDB)
+  .on('open', startServer)
